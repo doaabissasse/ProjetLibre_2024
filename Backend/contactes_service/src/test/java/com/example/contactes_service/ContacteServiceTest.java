812f50794e55b2b576765a87cc05ee8ac57fb7fd
salmaBoudehane <salmaboudehane2@gmail.com>
2024-11-29 18:52:13 +0000

    salma test

--- Backend/contactes_service/src/test/java/com/example/contactes_service/ContacteServiceTest.java
+++ Backend/contactes_service/src/test/java/com/example/contactes_service/ContacteServiceTest.java
@@ -53,7 +53,7 @@ class ContacteServiceTest {
     }
 
     @Test
-    void findAll_ReturnListOfContacts() {
+    void findAll_ListOfContacts() {
         // Arrange
         contacte contacte1 = contacte.builder()
                 .id(1L)
