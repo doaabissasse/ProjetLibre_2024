54af2608e02270a35069acbe4e01e99b0e3a0c45
salmaBoudehane <salmaboudehane2@gmail.com>
2025-01-01 19:41:29 +0000

    automate front

--- Backend/adresses_service/src/main/java/com/example/adresses_service/AdressesServiceApplication.java
+++ Backend/adresses_service/src/main/java/com/example/adresses_service/AdressesServiceApplication.java
@@ -2,10 +2,13 @@ package com.example.adresses_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableFeignClients // Précisez le package contenant les clients Feign
+@EnableConfigurationProperties
 public class AdressesServiceApplication {
 
 	public static void main(String[] args) {
