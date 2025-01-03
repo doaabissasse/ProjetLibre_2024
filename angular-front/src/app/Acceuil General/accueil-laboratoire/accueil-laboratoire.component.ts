b8ff1027ff989a858d41edf5ba390b97d6dcd37b
ksiham19 <sihamkhiar18@gmail.com>
2025-01-03 15:00:22 +0100

    lier examen avec dossier

--- angular-front/src/app/Acceuil General/accueil-laboratoire/accueil-laboratoire.component.ts
+++ angular-front/src/app/Acceuil General/accueil-laboratoire/accueil-laboratoire.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { LoginModalComponent } from "../login-modal/login-modal.component";
 import {RouterLink, RouterLinkActive} from "@angular/router";
-import { ContactMessage } from '../models/ContactMessage';
-import {ContactService} from '../services/ContactService.service';
 
 @Component({
   selector: 'app-accueil-laboratoire',
@@ -25,24 +23,8 @@ export class AccueilLaboratoireComponent {
   closeLoginModal() {
     this.isLoginModalVisible = false;
   }
-  constructor(private contactService: ContactService) {}
-  sendMessage(): void {
-    const contactMessage: ContactMessage = {
-      name: this.name,
-      email: this.email,
-      message: this.message
-    };
-    this.contactService.sendMessage(contactMessage).subscribe(
-      response => {
-        console.log('Message envoyé avec succès:', response);
-        // Réinitialiser le formulaire ou afficher un message de succès
-        this.name = '';
-        this.email = '';
-        this.message = '';
-      },
-      error => {
-        console.error('Erreur lors de l\'envoi du message:', error);
-      }
-    );
+
+  sendMessage(){
+
   }
   }
